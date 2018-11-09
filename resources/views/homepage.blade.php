eb7495688df0970863dca39d4340a8c362ee4671
Joey Westenberg <jojowestenberg@gmail.com>
2018-11-09 15:49:29 +0100

    Laravel Webiste v0.5

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('header')
+    <h2>Mediacollege Amsterdam</h2>
+@endsection
+
+@section('content')
+    Dit is artikel ...
+@endsection
+
+@section('content')
+    Dit is artikel ...
+@endsection
